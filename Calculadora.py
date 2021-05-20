300e6d6520ff693e67ab7206d80cc5ab080029ad
Pedro Henrique Veloso Assis <83790785+pedrohveloso@users.noreply.github.com>
2021-05-19 21:40:03 -0300

    Desafio da Calculadora
    O desafio consiste em criar uma calculadora usando POO. 
    Ela deve conter os seguintes métodos:
    - Soma
    - Subtração
    - Multiplicação
    - Divisão
    Esses métodos funcionam apenas com dois valores e todos devem retornar o valor.
    Os valores devem ser passados por parâmetro.

---
+++ Calculadora.py
@@ -0,0 +1,18 @@
+class Calculadora(object):
+
+        
+    def somar(self, primeiro_valor, segundo_valor):
+        resultado = primeiro_valor + segundo_valor
+        print(f"O resultado é {resultado}")
+    
+    def subtrair(self, primeiro_valor, segundo_valor):
+        resultado = primeiro_valor - segundo_valor
+        print(f"O resultado é {resultado}")
+    
+    def multiplicar(self, primeiro_valor, segundo_valor):
+        resultado = primeiro_valor * segundo_valor
+        print(f"O resultado é {resultado}")
+        
+    def dividir(self, primeiro_valor, segundo_valor):
+        resultado = primeiro_valor / segundo_valor
+        print(f"O resultado é {resultado}")
